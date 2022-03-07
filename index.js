0e6164a42da24d29c55e2c1d982d605ab3783d1c
Eduardo Miguel Victoria Sánchez <eduardo_mvs99@outlook.com>
2022-03-06 18:31:01 -0600

    Final result of CHALLENGE 1

---
+++ index.js
@@ -0,0 +1,13 @@
+/** JS-BASIC/INTRO/CHALLENGE-1 -TASK
+ * 
+ * Create new .js file called "other.js" 
+ * and in this file following line should
+ * be printed to the console:
+ * 
+ * "Greetings from other.js file!"
+ * 
+ * Add referenceto this file to the 
+ * "index.html"
+*/
+
+console.log("Hello from external JavaScript file");
